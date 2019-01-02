8b2c5ec1011fd063360016d39050ec718ce5284d
Jeroen Arne Wichers <jwichers@hotmail.com>
2019-01-02 13:18:28 +0100

    first version

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+<body>
+
+<h1> CV Arne</h1>
+
+Personalia:
+
+<?php
+
+$voornaam = "Jeroen";
+$achternaam = "Wichers";
+$leeftijd = "28";
+$woonplaats = "Enschede";
+$email = "jwichers@hotmail.com";
+$github = "https://github.com/JeroenArneWichers";
+
+echo $voornaam." ".$achternaam.",".$leeftijd.",".$woonplaats.",".$email.",".$github;
+
+?>
+
+</body>
+
+</html>
